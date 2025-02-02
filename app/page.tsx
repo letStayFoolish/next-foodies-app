185f3a2bb98f6b14f931604dee0a162daeaf8879
stayFoolish <nemanjakaraklajic90@icloud.com>
2025-02-02 17:20:26 +0100

    Add image slideshow component to homepage header
    Introduced a client-side image slideshow component showing rotating images with smooth transitions. Integrated the component into the homepage header, enhancing the visual appeal and user experience. Additional styles were added to support the slideshow functionality.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Link from "next/link";
 import classes from "./page.module.css";
+import ImagesSlideshow from "@/components/images/images-slideshow";
+
+
 const Home: React.FC = ()=> {
-  return (
+    console.log("RSC React Server Component - render only on the server");
+
+    return (
       <>
           <header className={classes.header}>
-              <div className={classes.slideshow}></div>
+              <div className={classes.slideshow}>
+                  <ImagesSlideshow />
+              </div>
               <div>
                   <div className={classes.hero}>
                       <h1>NextLevel Food for NextLevel Foodies</h1>
