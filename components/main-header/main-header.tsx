9d4367c442cd7e0e5678c50749b35ff93ef66b24
stayFoolish <nemanjakaraklajic90@icloud.com>
2025-02-02 16:52:50 +0100

    Refactor MainHeader component structure and styles.
    Separated header background into its own component and CSS module for better modularity. Moved and renamed MainHeader-related files into a dedicated folder to improve organization. Adjusted imports and layout references accordingly.

---
+++ components/main-header/main-header.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Link from "next/link";
+import logoImage from "@/assets/logo.png"
+import classes from "./main-header.module.css";
+import Image from "next/image";
+import MainHeaderBackground from "@/components/main-header/main-header-background";
+
+const MainHeader: React.FC = () => {
+    return (
+        <>
+            <MainHeaderBackground />
+            <header className={classes.header}>
+                <Link className={classes.logo} href="/">
+                    <Image src={logoImage} alt="a plate with food on it" priority />
+                    Next Level Food
+                </Link>
+                <nav className={classes.nav}>
+                    <ul>
+                        <li>
+                            <Link href="/meals">Browse Meals</Link>
+                        </li>
+                        <li>
+                            <Link href="/community">Foodies Community</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+
+    );
+};
+
+export default MainHeader;
