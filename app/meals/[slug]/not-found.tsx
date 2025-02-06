07c2a1de7e0230538490f38504935cf00e113df8
nemanja.karaklajic <nemanjakaraklajic@teletrader.com>
2025-02-06 12:29:38 +0100

    Add not-found page and handle missing meals gracefully
    Introduced a `not-found` page for meals and updated the meal page logic to redirect to the new page when a meal is not found. Ensured code now avoids rendering undefined data by verifying meal existence upfront.

---
+++ app/meals/[slug]/not-found.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <main className="not-found">
+            <h1>Meal not found</h1>
+            <p>Sorry, we can't find the meal you are looking for</p>
+        </main>
+    );
+};
+
+export default NotFound;
--- app/meals/[slug]/page.tsx
+++ app/meals/[slug]/page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import classes from "./page.module.css"
 import {getMeal} from "@/lib/meals";
 import Image from "next/image"
+import {notFound} from "next/navigation";
 
 type Props = {
     params: Promise<{
@@ -16,9 +17,12 @@ const Meal: React.FC<Props> = async ({params}) => {
 
     const meal = await getMeal(slug)
 
-    const instructions = meal?.instructions?.replace(/\n/g, "<br />") as string;
+    if(!meal) {
+        return notFound();
+    }
 
-    console.log({meal})
+    const instructions = meal.instructions?.replace(/\n/g, "<br />") as string;
+
 
     return (
       <>
@@ -27,12 +31,12 @@ const Meal: React.FC<Props> = async ({params}) => {
               <Image src={meal?.image ?? ""} alt={meal?.title ?? ""} fill />
           </div>
           <div className={classes.headerText}>
-              <h1>{meal?.title}</h1>
+              <h1>{meal.title}</h1>
               <p className={classes.creator}>
-                  by <a href={`mailto:${meal?.creator_email}`}>{meal?.creator}</a>
+                  by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
               </p>
               <p className={classes.summary}>SUMMARY</p>
-              <p>{meal?.summary}</p>
+              <p>{meal.summary}</p>
           </div>
       </header>
           <main>
